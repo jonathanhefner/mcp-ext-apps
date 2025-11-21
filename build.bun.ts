7d38af9bbb34e6c767b75b7a8d4c58d930fcae7f
Olivier Chafik <ochafik@anthropic.com>
2025-11-21 19:28:45 +0000

    build w/ bun to avoid internal deps hell

---
+++ build.bun.ts
@@ -0,0 +1,27 @@
+#!/usr/bin/env bun
+import { $ } from "bun";
+
+// Run TypeScript compiler for type declarations
+await $`tsc`;
+
+const isDevelopment = Bun.env.NODE_ENV === "development";
+
+// Build all JavaScript/TypeScript files
+function buildJs(entrypoint: string, opts: Record<string, string> = {}) {
+    return Bun.build({
+        entrypoints: [entrypoint],
+        outdir: "dist",
+        target: "browser",
+        minify: !isDevelopment,
+        ...(isDevelopment ? {
+            sourcemap: "inline",
+        } : {}),
+        ...opts
+    })
+}
+
+await Promise.all([
+  buildJs("src/app.ts", { outdir: "dist/src" }),
+  buildJs("src/app-bridge.ts", { outdir: "dist/src" }),
+  buildJs("src/react/index.tsx", { outdir: "dist/src/react" }),
+])
